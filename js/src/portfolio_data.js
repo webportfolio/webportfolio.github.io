d763a57f74a030dec6306efc68f7dd0a13bee58b
xidorami <erivery2@gmail.com>
2016-12-23 12:36:24 +0900

    수강생 웹 포폴 기본 사이트연결 구축_

---
+++ js/src/portfolio_data.js
@@ -0,0 +1,86 @@
+
+	var base = "./img/portfolio/suwon_1606/";
+	var imgAlt = "수강생 포트폴리오 이미지";
+	var person = "개인포트폴리오 ";
+
+	var useProgram = "Photoshop, illustrator, html, css, jQuery";
+	var responsive = "mobile, tablet, pc (반응형 웹)";
+	var pc ="pc";
+	var pf = [
+		{"study":"2016.06 mobile UI/UX 과정",
+		"student":
+		[
+		// 수강생_1: 이재영 -----------------------------------------------------
+			{"img":base + "leejy.jpg",	"imgAlt":imgAlt,
+			"name":"이재영",
+			"site":person + "https://wodud222.github.io",
+			"useProgram":useProgram,
+			"etc":responsive,
+			"link":"https://wodud222.github.io"},
+		// 수강생_2: 윤혜림 -----------------------------------------------------
+			{"img":base + "yunherim.jpg",	"imgAlt":imgAlt,
+			"name":"윤혜림",
+			"site":"미샤 리뉴얼" + "https://yunhyerim.github.io",
+			"useProgram":responsive,
+			"etc":responsive,
+			"link":"https://yunhyerim.github.io"},
+		// 수강생_3: 변은진 -----------------------------------------------------
+			{"img":base + "byunej.jpg",	"imgAlt":imgAlt,
+			"name":"변은진",
+			"site":person + "https://byunej.github.io/",
+			"useProgram":useProgram,
+			"etc":"pc",
+			"link":"https://byunej.github.io/"},
+		// 수강생_4: 김지현 -----------------------------------------------------
+			{"img":base + "kimjh.jpg",	"imgAlt":imgAlt,
+			"name":"김지현",
+			"site":"새마을 식당 리뉴얼"+"https://kimjih26.github.io/",
+			"useProgram":useProgram,
+			"etc":pc,
+			"link":"https://kimjih26.github.io"},
+		// 수강생_5: 이은영 -----------------------------------------------------
+			{"img":base + "leeej.jpg",	"imgAlt":imgAlt,
+			"name":"이은영",
+			"site":person + "https://papillion8.github.io/",
+			"useProgram":"Photoshop, illustrator, html, css, jQuery",
+			"etc":"pc",
+			"link":"https://papillion8.github.io"},
+		// 수강생_6: 홍은지 -----------------------------------------------------
+			{"img":base + "hongej.jpg",	"imgAlt":imgAlt,
+			"name":"홍은지",
+			"site":person + "https://honglama.github.io/pf/",
+			"useProgram":"Photoshop, illustrator, html, css, jQuery",
+			"etc":pc,
+			"link":"https://honglama.github.io/pf/"},
+		// 수강생_7: 유혜인 -----------------------------------------------------
+			{"img":base + "youhi.jpg",	"imgAlt":imgAlt,
+			"name":"유혜인",
+			"site":"토니모리 리뉴얼 "+"https://youhyein.github.io/tonymoly",
+			"useProgram":responsive,
+			"etc":pc,
+			"link":"https://youhyein.github.io/tonymoly"},
+		// 수강생_8: 이재형 -----------------------------------------------------
+			{"img":base + "kimjs.jpg",	"imgAlt":imgAlt,
+			"name":"이재형",
+			"site":"리바이스 리뉴얼"+"https://puky1027.github.io/",
+			"useProgram":"Photoshop, illustrator, html, css, jQuery",
+			"etc":pc,
+			"link":"https://puky1027.github.io/"},
+		// 수강생_9: 김지수 -----------------------------------------------------
+			{"img":base + "kimjs.jpg",	"imgAlt":imgAlt,
+			"name":"김지수",
+			"site":"디자인 웨딩"+"https://rlawltn5891.github.io/www/",
+			"useProgram":"Photoshop, illustrator, html, css, jQuery",
+			"etc":pc,
+			"link":"https://rlawltn5891.github.io/www/"},
+		// 수강생_10: 현주희 -----------------------------------------------------
+			// {"img":base + "test_02.jpg",	"imgAlt":imgAlt,
+			// "name":"현주희",
+			// "site":"개인포트폴리오 페이지",
+			// "useProgram":"Photoshop, illustrator, html, css, jQuery",
+			// "etc":pc,
+			// "link":"http://"},
+		]} // study과정1
+	];  
+
+
---
+++ js/src/portfolio_list.js
@@ -0,0 +1,38 @@
+$(function() {
+
+var contentBox = $('#contentBox');
+var content;
+ var i = 0;
+
+console.log(pf.length);
+
+ for(; i<pf.length; i++){
+ 	console.log(i);
+ content = contentBox.append('<article class="content">');
+ 		student(i);
+ }
+
+function student(i){
+		 $('.content').append('<h3>'+ pf[i].study+'</h3>');
+
+     var j = 0;
+     for(; j<pf[i].student.length; j++){
+     	console.log(pf[i].student[j].link);
+     $('.content').append('<div class="student">');
+     $('.student').eq(j).append('<div class="web_img"><img>');
+     $('.student').eq(j).append('<dl class="portfolio_info">');
+		 $('.student').eq(j).find('img').attr({'src':pf[i].student[j].img, 'alt':pf[i].student[j].imgAlt});
+     var studentLast = $('.student').find('dl').last();
+     studentLast.append('<dt><strong>이름 :</strong>'+ pf[i].student[j].name +'</dt>');
+     studentLast.append('<dd><strong>사이트 주소 :</strong>'+ pf[i].student[j].site +'</dd>');
+     studentLast.append('<dd><strong>사용 프로그램 :</strong>'+ pf[i].student[j].useProgram +'</dd>');
+     studentLast.append('<dd><strong>디바이스(해상도) :</strong>'+ pf[i].student[j].etc +'</dd>');
+
+
+
+     studentLast.append('<dd><a>사이트 바로가기</a></dd>');
+	   $('.student').eq(j).find('a').attr({"href":pf[i].student[j].link, "target":"_blank"});
+	 }
+	}
+});
+
