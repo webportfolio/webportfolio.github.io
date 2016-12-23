a90916e308e4fafe9951907509c80dbdfa3020d5
xidorami <erivery2@gmail.com>
2016-12-23 18:13:26 +0900

    디바이스 내용 변경

--- js/src/portfolio_data.js
+++ js/src/portfolio_data.js
@@ -1,5 +1,9 @@
 
-	var base = "./img/portfolio/suwon_1606/";
+	var base = "./img/portfolio/";
+	var missimg = "webpf_img.svg";
+	var suwon01 = base+"suwon_1606/";
+	var gangnam1602 = base+"gangnam_1602";
+
 	var imgAlt = "수강생 포트폴리오 이미지";
 	var person = "개인포트폴리오 ";
 
@@ -7,93 +11,256 @@
 	var responsive = "mobile, tablet, pc (반응형 웹)";
 	var pc ="pc";
 	var pf = [
-		{"study":"2016.06 mobile UI/UX 과정",
+// study과정5 ============================================================
+		{"study":"2016.06 <br/>Mobile UI/UX 과정",
 		"student":
 		[
 		// 수강생_1: 이재영 -----------------------------------------------------
-			{"img":base + "leejy.jpg",	"imgAlt":imgAlt,
+			{"img":suwon01 + "leejy.jpg",	"imgAlt":imgAlt,
 			"name":"이재영",
-			"site":person + "https://wodud222.github.io",
+			"site":person,
 			"useProgram":useProgram,
 			"etc":responsive,
 			"link":"https://wodud222.github.io"},
 		// 수강생_2: 윤혜림 -----------------------------------------------------
-			{"img":base + "yunherim.jpg",	"imgAlt":imgAlt,
+			{"img":suwon01 + "yunherim.jpg",	"imgAlt":imgAlt,
 			"name":"윤혜림",
-			"site":"미샤 리뉴얼" + "https://yunhyerim.github.io",
-			"useProgram":responsive,
+			"site":"미샤 리뉴얼",
+			"useProgram":useProgram,
 			"etc":responsive,
 			"link":"https://yunhyerim.github.io"},
 		// 수강생_3: 변은진 -----------------------------------------------------
-			{"img":base + "byunej.jpg",	"imgAlt":imgAlt,
+			{"img":suwon01 + "byunej.jpg",	"imgAlt":imgAlt,
 			"name":"변은진",
-			"site":person + "https://byunej.github.io/",
+			"site":person,
 			"useProgram":useProgram,
 			"etc":"pc",
 			"link":"https://byunej.github.io/"},
 		// 수강생_4: 김지현 -----------------------------------------------------
-			{"img":base + "kimjh.jpg",	"imgAlt":imgAlt,
+			{"img":suwon01 + "kimjh.jpg",	"imgAlt":imgAlt,
 			"name":"김지현",
-			"site":"새마을 식당 리뉴얼"+"https://kimjih26.github.io/",
+			"site":"새마을 식당 리뉴얼",
 			"useProgram":useProgram,
 			"etc":pc,
 			"link":"https://kimjih26.github.io"},
 		// 수강생_5: 이은영 -----------------------------------------------------
-			{"img":base + "leeej.jpg",	"imgAlt":imgAlt,
+			{"img":suwon01 + "leeej.jpg",	"imgAlt":imgAlt,
 			"name":"이은영",
-			"site":person + "https://papillion8.github.io/",
+			"site":person,
 			"useProgram":"Photoshop, illustrator, html, css, jQuery",
 			"etc":"pc",
 			"link":"https://papillion8.github.io"},
 		// 수강생_6: 홍은지 -----------------------------------------------------
-			{"img":base + "hongej.jpg",	"imgAlt":imgAlt,
+			{"img":suwon01 + "hongej.jpg",	"imgAlt":imgAlt,
 			"name":"홍은지",
-			"site":person + "https://honglama.github.io/pf/",
+			"site":person,
 			"useProgram":"Photoshop, illustrator, html, css, jQuery",
 			"etc":pc,
 			"link":"https://honglama.github.io/pf/"},
 		// 수강생_7: 유혜인 -----------------------------------------------------
-			{"img":base + "youhi.jpg",	"imgAlt":imgAlt,
+			{"img":suwon01 + "youhi.jpg",	"imgAlt":imgAlt,
 			"name":"유혜인",
-			"site":"토니모리 리뉴얼 "+"https://youhyein.github.io/tonymoly",
+			"site":"토니모리 리뉴얼",
 			"useProgram":responsive,
 			"etc":pc,
 			"link":"https://youhyein.github.io/tonymoly"},
 		// 수강생_8: 이재형 -----------------------------------------------------
-			{"img":base + "leejh.jpg",	"imgAlt":imgAlt,
+			{"img":suwon01 + "leejh.jpg",	"imgAlt":imgAlt,
 			"name":"이재형",
-			"site":"리바이스 리뉴얼"+"https://puky1027.github.io/",
+			"site":"리바이스 리뉴얼",
 			"useProgram":"Photoshop, illustrator, html, css, jQuery",
 			"etc":pc,
 			"link":"https://puky1027.github.io/"},
 		// 수강생_9: 김지수 -----------------------------------------------------
-			{"img":base + "kimjs.jpg",	"imgAlt":imgAlt,
+			{"img":suwon01 + "kimjs.jpg",	"imgAlt":imgAlt,
 			"name":"김지수",
-			"site":"디자인 웨딩"+"https://rlawltn5891.github.io/www/",
+			"site":"디자인 웨딩",
 			"useProgram":"Photoshop, illustrator, html, css, jQuery",
 			"etc":pc,
 			"link":"https://rlawltn5891.github.io/www/"},
 		// 수강생_10: 현주희 -----------------------------------------------------
-			// {"img":base + "test_02.jpg",	"imgAlt":imgAlt,
+			// {"img":suwon01 + "test_02.jpg",	"imgAlt":imgAlt,
 			// "name":"현주희",
 			// "site":"개인포트폴리오 페이지",
 			// "useProgram":"Photoshop, illustrator, html, css, jQuery",
 			// "etc":pc,
 			// "link":"http://"},
-		]}, // study과정1
+		]}, 
 
-		// study과정2 ============================================================
-		// {"study":"2016.02 webdesign 과정",
-		// "student":
-		// [
-		// // 수강생_1: 이재영 -----------------------------------------------------
-		// 	{"img":base + "leejy.jpg",	"imgAlt":imgAlt,
-		// 	"name":"이재영",
-		// 	"site":person + "https://wodud222.github.io",
-		// 	"useProgram":useProgram,
-		// 	"etc":responsive,
-		// 	"link":"https://wodud222.github.io"}
-		// 	]}
+// study과정4 ============================================================
+		{"study":"2016.02 <br/>Webdesign 과정",
+		"student":
+		[
+		// 수강생_1: 정소담 -----------------------------------------------------
+			{"img":/*gangnam1602 +*/ base + missimg,	"imgAlt":imgAlt,
+			"name":"정소담",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":pc,
+			"link":"http://damso0204.dothome.co.kr "},
+		// 수강생_2: 김지선 -----------------------------------------------------	
+			{"img":/*gangnam1602 + */base + missimg,	"imgAlt":imgAlt,
+			"name":"김지선",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":responsive,
+			"link":"http://kimjs9105.github.io"},
+		// 수강생_3: 최형선 -----------------------------------------------------	
+			{"img":/*gangnam1602 + */base + missimg,	"imgAlt":imgAlt,
+			"name":"최형선",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":responsive,
+			"link":"http://hyungsun.github.io/"},
+		// 수강생_4: 이혜정 -----------------------------------------------------	
+			{"img":/*gangnam1602 + */base + missimg,	"imgAlt":imgAlt,
+			"name":"이혜정",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":responsive,
+			"link":"http://falkore.dothome.co.kr/"},
+		// 수강생_5: 김은혜 -----------------------------------------------------	
+			{"img":/*gangnam1602 + */base + missimg,	"imgAlt":imgAlt,
+			"name":"김은혜",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":pc,
+			"link":"http://catwitch.dothome.co.kr/"},
+		// 수강생_6: 김민선 -----------------------------------------------------	
+			{"img":/*gangnam1602 + */base + missimg,	"imgAlt":imgAlt,
+			"name":"김민선",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":responsive,
+			"link":"http://kimminseon.github.io/alstjs1595"},
+		// 수강생_7: 김슬기 -----------------------------------------------------	
+			{"img":/*gangnam1602 + */base + missimg,	"imgAlt":imgAlt,
+			"name":"김슬기",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":pc,
+			"link":"http://tmfrldbsrn.dothome.co.kr"},
+		// 수강생_8: 간은선 -----------------------------------------------------	
+			{"img":/*gangnam1602 + */base + missimg,	"imgAlt":imgAlt,
+			"name":"간은선",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":responsive,
+			"link":"http://kaneunsun.dothome.co.kr/"},
+		// 수강생_9: 조은진 -----------------------------------------------------	
+			{"img":/*gangnam1602 + */base + missimg,	"imgAlt":imgAlt,
+			"name":"조은진",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":pc,
+			"link":"http://dmswls1903.github.io/dmswls1903/"}
+			]},
+// study과정3 ============================================================	
+			{"study":"2016.03 <br/>Webdesign 과정",
+		"student":
+		[
+		// 수강생_1: 이희애 -----------------------------------------------------
+			{"img":/*gangnam1602 +*/ base + missimg,	"imgAlt":imgAlt,
+			"name":"이희애",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":responsive,
+			"link":"http://heeae0302.dothome.co.kr/"},
+		// 수강생_2: 이승엽 -----------------------------------------------------
+			{"img":/*gangnam1602 +*/ base + missimg,	"imgAlt":imgAlt,
+			"name":"이승엽",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":responsive,
+			"link":"http://suql0702.dothome.co.kr"},
+		// 수강생_3: 황진경 -----------------------------------------------------
+			{"img":/*gangnam1602 +*/ base + missimg,	"imgAlt":imgAlt,
+			"name":"황진경",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":responsive,
+			"link":"http://hwangjk711.github.io/"},
+		// 수강생_4: 김미나 -----------------------------------------------------
+			{"img":/*gangnam1602 +*/ base + missimg,	"imgAlt":imgAlt,
+			"name":"김미나",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":responsive,
+			"link":"http://kke0807.github.io/"},
+		// 수강생_5: 신종원 -----------------------------------------------------
+			{"img":/*gangnam1602 +*/ base + missimg,	"imgAlt":imgAlt,
+			"name":"신종원",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":pc,
+			"link":"http://mnweb.dothome.co.kr/"},
+		// 수강생_6: 박세용 -----------------------------------------------------
+			{"img":/*gangnam1602 +*/ base + missimg,	"imgAlt":imgAlt,
+			"name":"박세용",
+			"site":"YMC entertainment 리뉴얼",
+			"useProgram":useProgram,
+			"etc":pc,
+			"link":"http://seyong0290.dothome.co.kr/"},
+		// 수강생_7: 최윤혜 -----------------------------------------------------
+			{"img":/*gangnam1602 +*/ base + missimg,	"imgAlt":imgAlt,
+			"name":"최윤혜",
+			"site":"Ziwi peak(애완동물 식재료) 리뉴얼",
+			"useProgram":useProgram,
+			"etc":pc,
+			"link":"http://kommang4.dothome.co.kr/"},
+		// 수강생_8: 송예빈 -----------------------------------------------------
+			{"img":/*gangnam1602 +*/ base + missimg,	"imgAlt":imgAlt,
+			"name":"송예빈",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":pc,
+			"link":"http://songyebin.github.io/"},
+		]},
+// study과정2 ============================================================	
+			{"study":"2015.06 <br/>Webdesign 과정",
+		"student":
+		[
+		// 수강생_1: 이선주 -----------------------------------------------------
+			{"img":/*gangnam1602 +*/ base + missimg,	"imgAlt":imgAlt,
+			"name":"이선주",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":responsive,
+			"link":"http://gngn97.dothome.co.kr/"},
+		// 수강생_2: 김지혜 -----------------------------------------------------
+			{"img":/*gangnam1602 +*/ base + missimg,	"imgAlt":imgAlt,
+			"name":"김지혜",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":responsive,
+			"link":"http://kjh9207.dothome.co.kr/"},
+		// 수강생_3: 김지혜 -----------------------------------------------------
+			{"img":/*gangnam1602 +*/ base + missimg,	"imgAlt":imgAlt,
+			"name":"김지혜",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":responsive,
+			"link":"http://kjh9207.dothome.co.kr/"},
+		// 수강생_4: 유주원 -----------------------------------------------------
+			{"img":/*gangnam1602 +*/ base + missimg,	"imgAlt":imgAlt,
+			"name":"유주원",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":responsive,
+			"link":"http://silbap.dothome.co.kr/"},
+			]},
+// study과정1 ============================================================	
+		{"study":"2015.05 <br/>Webdesign 과정",
+		"student":
+		[
+		// 수강생_1: 이선주 -----------------------------------------------------
+			{"img":/*gangnam1602 +*/ base + missimg,	"imgAlt":imgAlt,
+			"name":"이선주",
+			"site":person,
+			"useProgram":useProgram,
+			"etc":responsive,
+			"link":"http://gngn97.dothome.co.kr/"},
+			]}
 	];  
 
 
--- js/src/portfolio_list.js
+++ js/src/portfolio_list.js
@@ -5,7 +5,7 @@ var content;
 var i = 0;
 var timed = 500;
 
-console.log(pf.length);
+// console.log(pf.length);
 
  for(; i<pf.length; i++){
  	// console.log(i);
@@ -18,26 +18,27 @@ function student(i){
 
    var j = 0;
    for(; j<pf[i].student.length; j++){
-   	console.log(pf[i].student[j].link);
+   	// console.log(pf[i].student[j].link);
    $('.content').eq(i).append('<div class="student">');
    var content_student = $('.content').eq(i).find('.student');
    content_student.eq(j).append('<div class="web_img"><img>');
    content_student.eq(j).append('<dl class="portfolio_info">');
 	 content_student.eq(j).find('img').attr({'src':pf[i].student[j].img, 'alt':pf[i].student[j].imgAlt});
    var studentLast = $('.student').find('dl').last();
-   studentLast.append('<dt><strong>이름 :</strong>'+ pf[i].student[j].name +'</dt>');
-   studentLast.append('<dd><strong>사이트 주소 :</strong>'+ pf[i].student[j].site +'</dd>');
-   studentLast.append('<dd><strong>사용 프로그램 :</strong>'+ pf[i].student[j].useProgram +'</dd>');
-   studentLast.append('<dd><strong>디바이스(해상도) :</strong>'+ pf[i].student[j].etc +'</dd>');
+   studentLast.append('<dt><strong>이름 :</strong><br />'+ pf[i].student[j].name +'</dt>');
+   studentLast.append('<dd><strong>사이트 구분 :</strong><br />'+ pf[i].student[j].site +'</dd>');
+   studentLast.append('<dd><strong>사용 프로그램 :</strong><br />'+ pf[i].student[j].useProgram +'</dd>');
+   studentLast.append('<dd><strong>디바이스(해상도) :</strong><br />'+ pf[i].student[j].etc +'</dd>');
 
 
 
    studentLast.append('<dd><a>사이트<br /> 바로가기</a></dd>');
-   $('.student').eq(j).find('a').attr({"href":pf[i].student[j].link, "target":"_blank"});
+   content_student.eq(j).find('a').attr({"href":pf[i].student[j].link, "target":"_blank"});
 	 }
 }
-// $('h3').siblings('.student').hide();
-// $('h3').removeClass('view');
+var content = $('.content');
+content.find('h3').removeClass('view').siblings('.student').hide();
+content.eq(0).find('h3').addClass('view').siblings('.student').show();
 // 
 $('h3').css({cursor:'pointer'});
   $('h3').on('click', function(){
